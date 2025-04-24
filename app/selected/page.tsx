9146faeaa0ccd0a205f4e6271aa90a03c59ed3df
Joelle Hili <joelle.hili@outlook.com>
2025-04-24 20:26:14 +0200

    chore: Added Selected Page Structure

---
+++ app/selected/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { SelectedTypes } from "./types/selectedTypes";
+
+export default function Selected({ symbol }: SelectedTypes) {
+
+    console.log("Selected Company:", symbol)
+
+    return (
+        <main>
+        </main>
+    );
+}
