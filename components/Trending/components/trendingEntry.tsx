009040659621d86369458fc90b3917fb7f724e8a
Joelle Hili <joelle.hili@outlook.com>
2025-04-26 09:10:47 +0200

    fix: Trending Navigation not sending data properly

--- components/Trending/components/trendingEntry.tsx
+++ components/Trending/components/trendingEntry.tsx
@@ -3,7 +3,7 @@
 import { TrendingTypes } from "../types/trendingTypes"
 
 const TrendingEntry = ({symbol, displayName}: TrendingTypes) => (
-    <a className="trending__entry" href={`/search?${symbol}`}>
+    <a className="trending__entry" href={`/search?ticker=${symbol}`}>
         <span className="trending__entry__symbol">{symbol}</span>
         <span className="trending__entry__name">{displayName}</span>
     </a>
